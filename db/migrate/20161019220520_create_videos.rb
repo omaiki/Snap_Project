5dc03a024756330df60c5f662dd7dfc5b619b79b
Oshoke Maiki <oshokemaiki@gmail.com>
2016-10-19 15:09:50 -0700

    Create migrations/models for User, Video, Screenshot with associations

---
+++ db/migrate/20161019220520_create_videos.rb
@@ -0,0 +1,10 @@
+class CreateVideos < ActiveRecord::Migration[5.0]
+  def change
+    create_table :videos do |t|
+      t.text :url
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
