fc7b3ac59eb36bb7de44a5dd0218e9bb1d1d47a8
Oshoke Maiki <oshokemaiki@gmail.com>
2016-11-08 14:48:54 -0800

    Delete video resource form routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
 
   resources :users
   resources :sessions
-  resources :videos
   resources :screenshots
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
